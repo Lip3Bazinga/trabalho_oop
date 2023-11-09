8223db4719afd6ee0e887207e811c807a6de5a91
Alexandre Santato <alexandresantato2@gmail.com>
2023-11-09 16:17:42 -0300

    teste dnv

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-class Main{
-  public static void main(String[] args){
-    System.out.println("Clever micropenis");
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Clever teste");
   }
 }
