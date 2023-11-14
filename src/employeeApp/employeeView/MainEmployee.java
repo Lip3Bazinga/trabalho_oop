4981158f0784d4db3e74e7beca6ce6c77991d1df
Alexandre Santato <alexandresantato2@gmail.com>
2023-11-14 19:18:48 -0300

    Adição das Views

---
+++ src/employeeApp/employeeView/MainEmployee.java
@@ -0,0 +1,7 @@
+package employeeApp.employeeView;
+
+public class MainEmployee {
+    public static void main(String args[]) {
+        System.out.println("main 2");
+    }
+}
---
+++ src/managerApp/managerView/MainManager.java
@@ -0,0 +1,7 @@
+package managerApp.managerView;
+
+public class MainManager {
+    public static void main(String args[]) {
+        System.out.println("main 1");
+    }
+}
