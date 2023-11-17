b55dbc9e08a33bb6fa5dbb8291735d61b58aae59
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 15:58:16 -0300

    Update NonfleetVehicleException.java

--- src/managerApp/managerExceptions/NonfleetVehicleException.java
+++ src/managerApp/managerExceptions/NonfleetVehicleException.java
@@ -1,5 +1,8 @@
 package managerApp.managerExceptions;
 
+/**
+Classe para exceção veículo não faz parte da frota (informa que veículo não faz parte da frota). Excluir veículo existente 
+*/
 public class NonfleetVehicleException extends Exception {
     
     
