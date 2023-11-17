4ffc8f98606d55cd38b212fce583d5b5559d0d8d
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 16:52:00 -0300

    Update InvalidInputTypeException.java

--- src/managerApp/managerExceptions/InvalidInputTypeException.java
+++ src/managerApp/managerExceptions/InvalidInputTypeException.java
@@ -1,1 +1,9 @@
-i
+package managerApp.managerExceptions;
+
+/**
+Classe para exceção de completou com letra (informar que é para digitar um número em tal parâmetro de tal grupo). Alterar parâmetros operacionais 
+*/
+public class InvalidInputTypeException extends Exception {
+    
+    
+}
