56d9e90250d5bbd18f358df9d5a1b20f7f60491a
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 15:52:37 -0300

    Update PlateAlreadyRegisteredException.java

--- src/managerApp/managerExceptions/PlateAlreadyRegisteredException.java
+++ src/managerApp/managerExceptions/PlateAlreadyRegisteredException.java
@@ -1,5 +1,8 @@
 package managerApp.managerExceptions;
 
+/**
+Classe para exceção de placa já cadastrada na frota (informa que o veículo já se encontra cadastrado no sistema). Cadastrar novo veículo 
+*/
 public class PlateAlreadyRegisteredException extends Exception {
     
     
