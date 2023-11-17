938e01233f6a1da5de5b06540ccdc3ce5a4a5800
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 16:01:42 -0300

    Update NotPostiveNumberException.java

--- src/managerApp/managerExceptions/NotPostiveNumberException.java
+++ src/managerApp/managerExceptions/NotPostiveNumberException.java
@@ -1,5 +1,8 @@
 package managerApp.managerExceptions;
 
+/**
+Classe para exceção de completou com valor negativo ou zero (informar que completou com valor negativo ou zero em tal parâmetro de tal grupo). Alterar parâmetros operacionais 
+*/
 public class NotPostiveNumberException extends Exception {
     
     
