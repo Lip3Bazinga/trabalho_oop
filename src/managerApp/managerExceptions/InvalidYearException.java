bc01634eb3cd577fbd26eb58789cbc166ce12f4d
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 15:54:44 -0300

    Update InvalidYearException.java

--- src/managerApp/managerExceptions/InvalidYearException.java
+++ src/managerApp/managerExceptions/InvalidYearException.java
@@ -1,5 +1,8 @@
 package managerApp.managerExceptions;
 
+/**
+Classe para exceção de ano inválido (informa que ano inválido). Cadastrar novo veículo 
+*/
 public class InvalidYearException extends Exception {
     
     
