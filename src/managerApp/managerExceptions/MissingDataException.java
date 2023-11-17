139f819b7ac2fcbf144387a600a78c42acc12593
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 15:53:41 -0300

    Update MissingDataException.java

--- src/managerApp/managerExceptions/MissingDataException.java
+++ src/managerApp/managerExceptions/MissingDataException.java
@@ -1,5 +1,8 @@
 package managerApp.managerExceptions;
 
+/**
+Classe para exceção de dados (placa, marca, modelo, cor, ano, grupo) faltantes (informa sobre os campos que precisam ser preenchidos) . Cadastrar novo veículo 
+*/
 public class MissingDataException extends Exception {
     
     
