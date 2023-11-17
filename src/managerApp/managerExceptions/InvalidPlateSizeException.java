b4347dc1d218e79b381e53e3d64b23f60ed6da59
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 15:51:34 -0300

    Update InvalidPlateSizeException.java

--- src/managerApp/managerExceptions/InvalidPlateSizeException.java
+++ src/managerApp/managerExceptions/InvalidPlateSizeException.java
@@ -1,5 +1,8 @@
 package managerApp.managerExceptions;
 
+/**
+Classe para exceção de tamanho da placa invalida (mostrar formatação correta). Cadastrar novo veículo e excluir veículo existente 
+*/
 public class InvalidPlateSizeException extends Exception {
     
     
