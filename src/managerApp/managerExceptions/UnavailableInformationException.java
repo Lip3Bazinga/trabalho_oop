adeda0eef22507b10def2ad9cb4f09e25a18b9ab
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 15:59:41 -0300

    Update UnavailableInformationException.java

--- src/managerApp/managerExceptions/UnavailableInformationException.java
+++ src/managerApp/managerExceptions/UnavailableInformationException.java
@@ -1,5 +1,8 @@
 package managerApp.managerExceptions;
 
+/**
+Classe para exceção de não há informações disponíveis para gerar um relatório estatístico para a categoria selecionada pelo gerente (informa que não há dados disponíveis para a categoria selecionada). Gerar relatório estatístico 
+*/
 public class UnavailableInformationsException extends Exception {
     
     
