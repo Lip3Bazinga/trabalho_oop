a922e9e78af4a0f4f0edb9f4f61747d164a9dfaf
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 15:56:52 -0300

    Update LeasedVehicleException.java

--- src/managerApp/managerExceptions/LeasedVehicleException.java
+++ src/managerApp/managerExceptions/LeasedVehicleException.java
@@ -1,5 +1,8 @@
 package managerApp.managerExceptions;
 
+/**
+Classe para exceção de placa de veículo locado (informa que veículo está locado e impede a ação de remoção). Excluir veículo existente 
+*/
 public class LeasedVehicleException extends Exception {
     
     
