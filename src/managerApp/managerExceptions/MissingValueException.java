69806d0b1b9c55b1e724ff18db0af55a6829b10f
FernandaIkegami <142702366+FernandaIkegami@users.noreply.github.com>
2023-11-17 16:00:47 -0300

    Update MissingValueException.java

--- src/managerApp/managerExceptions/MissingValueException.java
+++ src/managerApp/managerExceptions/MissingValueException.java
@@ -1,5 +1,8 @@
 package managerApp.managerExceptions;
 
+/**
+Classe para exceção de faltou completar com valor (informar que faltou completar o valor de tal parâmetro de tal grupo). Alterar parâmetros operacionais 
+*/
 public class MissingValueException extends Exception {
     
     
