37743c7144ae2e176dd9a16d2332aa1f4776dcc4
Filipe Magalhães <filipepio2005@hotmail.com>
2023-11-18 17:06:56 -0300

    Revert "Operational Parameters"
    This reverts commit c2247b4e6621cfc95afb9db2c92e719be258e44a.

--- src/managerApp/managerModel/ModelOperationalParameters.java
+++ src/managerApp/managerModel/ModelOperationalParameters.java
@@ -2,56 +2,50 @@ package managerApp.managerModel;
 
 public class ModelOperationalParameters {
 
-    public static void main(){
-        System.out.println("Socorro");
-    }
-
     //Registrar valores dos parametros operacionais de cada grupo de veiculo
     
-    // public void setDailyValue(String group, float dailyValue) {
+    public void setDailyValue(String group, float dailyValue) {
 
-    // }
+    }
 
-    // public void setTankValue(String group, float tankValue) {
+    public void setTankValue(String group, float tankValue) {
 
-    // }
+    }
 
-    // public void setExternalCleaningPrice(String group, float externalCleaningPrice) {
+    public void setExternalCleaningPrice(String group, float externalCleaningPrice) {
 
-    // }
+    }
 
-    // public void setInternalCleaningPrice(String group, float internalCleaningPrice) {
+    public void setInternalCleaningPrice(String group, float internalCleaningPrice) {
 
-    // }
+    }
 
-    // public void setInsuranceDailyValue(String group, float insuranceDailyValue) {
+    public void setInsuranceDailyValue(String group, float insuranceDailyValue) {
 
-    // }
+    }
 
-    // public float getDailyValue(String group) {
+    public float getDailyValue(String group) {
         
-    //     return (float)0.0;
-    // }
+        return (float)0.0;
+    }
 
-    // public float getTankValue(String group) {
+    public float getTankValue(String group) {
 
-    //     return (float)0.0;
-    // }
+        return (float)0.0;
+    }
 
-    // public float getExternalCleaningPrice(String group) {
+    public float getExternalCleaningPrice(String group) {
 
-    //     return (float)0.0;
-    // }
+        return (float)0.0;
+    }
 
-    // public float getInternalCleaningPrice(String group) {
+    public float getInternalCleaningPrice(String group) {
 
-    //     return (float)0.0;
-    // }
+        return (float)0.0;
+    }
 
-    // public float getInsuranceDailyValue(String group) {
+    public float getInsuranceDailyValue(String group) {
 
-    //     return (float)0.0;
-    // }
-
-
+        return (float)0.0;
+    }
 }
